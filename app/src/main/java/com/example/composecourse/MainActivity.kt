274f77f80cb25d981d4b239c2610aaa8022b30f6
Sagar <avhad.avhad@gmail.com>
2021-11-01 17:32:15 +0530

    Rows, Columns and Basic Sizing

--- app/src/main/java/com/example/composecourse/MainActivity.kt
+++ app/src/main/java/com/example/composecourse/MainActivity.kt
@@ -4,9 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -16,6 +14,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.unit.dp
 import com.example.composecourse.ui.theme.ComposeCourseTheme
 
 class MainActivity : ComponentActivity() {
@@ -29,13 +28,18 @@ class MainActivity : ComponentActivity() {
     @Preview
     @Composable
     fun Practice(){
-        Column(
+        Row(
             modifier = Modifier
-                .fillMaxSize()
+                //.fillMaxSize(0.5f)
+                .width(300.dp)
+                //.height(200.dp)
+                .fillMaxHeight()
                 .background(Color.Green),
-            horizontalAlignment = Alignment.CenterHorizontally
+            horizontalArrangement = Arrangement.SpaceAround,
+            verticalAlignment = Alignment.CenterVertically
         ) {
-            Text("Sagar")
+            Text("SAGAR")
+            Text("KISAN")
             Text("AVHAD")
         }
     }
