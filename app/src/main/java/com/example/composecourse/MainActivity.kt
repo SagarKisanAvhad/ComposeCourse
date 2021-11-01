09ca54c14d4cfc6a936a3c325e52d14a9218f048
Sagar <avhad.avhad@gmail.com>
2021-11-01 16:33:25 +0530

    understanding Main axis,Arrange and Cross axis, Align, background color, fillMaxSize

--- app/src/main/java/com/example/composecourse/MainActivity.kt
+++ app/src/main/java/com/example/composecourse/MainActivity.kt
@@ -3,29 +3,40 @@ package com.example.composecourse
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
 import com.example.composecourse.ui.theme.ComposeCourseTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Greeting("Sagar")
+            Practice()
         }
     }
-}
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-        Greeting("Sagar")
+    @Preview
+    @Composable
+    fun Practice(){
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.Green),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text("Sagar")
+            Text("AVHAD")
+        }
+    }
 }
